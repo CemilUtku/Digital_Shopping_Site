635b8fb3cdb40dd01973b0c7c1a1dcc9791c8dd7
CemilUtku <92306245+CemilUtku@users.noreply.github.com>
2024-04-24 10:27:00 +0300

    Update views.py

--- simple_shopping_site/simple_shopping_site/views.py
+++ simple_shopping_site/simple_shopping_site/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, redirect
-from .models import Product, Customer  # Customer modelini import edin
+from .models import Product, Customer 
 from .forms import CustomerForm, ProductForm
 
 def home(request):
     customers = Customer.objects.all()  # Müşterileri al
-    return render(request, 'home.html', {'customers': customers})  # home.html şablonunu kullan
+    return render(request, 'home.html', {'customers': customers})  
 
 def list_customers(request):
     customers = Customer.objects.all()
-    return render(request, 'list_customers.html', {'customers': customers})  # Yeni bir şablon oluşturun
+    return render(request, 'list_customers.html', {'customers': customers})  
 
 def create_customer(request):
     if request.method == 'POST':
