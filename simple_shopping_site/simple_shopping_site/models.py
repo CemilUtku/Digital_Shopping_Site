1bf3fcc07d9a908f210c116c7f8b32be3ba7aba8
Utku <utkucalci@gmail.com>
2024-05-29 04:02:56 +0300

    Final

--- simple_shopping_site/simple_shopping_site/models.py
+++ simple_shopping_site/simple_shopping_site/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField(default=0)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
-    stock = models.PositiveIntegerField(default=0)  # Yeni eklenen alan: stok miktarı
+    stock = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.name
@@ -13,16 +13,15 @@ class Product(models.Model):
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)  # Varsayılan olarak None belirlendi
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.name
 
 class Order(models.Model):
-    class Order(models.Model):
-        customer = models.ForeignKey(Customer, on_delete=models.CASCADE,
-                                     default=None)
-        date = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE,
+                                 default=None)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Order {self.id} by {self.customer.name}"
--- simple_shopping_site/simple_shopping_site/urls.py
+++ simple_shopping_site/simple_shopping_site/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from simple_shopping_site import views as simple_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,4 +18,4 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', simple_views.signup, name='signup'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
