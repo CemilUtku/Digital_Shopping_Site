ea13c16e86261fc772f7fb39c0c98711b7463771
Utku <utkucalci@gmail.com>
2024-05-29 10:46:12 +0300

    End

--- simple_shopping_site/simple_shopping_site/settings.py
+++ simple_shopping_site/simple_shopping_site/settings.py
@@ -99,7 +99,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "simple_shopping_site/static"),
+    'C:/Users/asus/PycharmProjects/Digital_Shopping_Site/simple_shopping_site/simple_shopping_site/static/'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
